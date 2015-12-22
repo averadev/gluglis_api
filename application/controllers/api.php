9c302f74ddfa7619a1327df1e9cded3a4863d174
conomia <conomia_alfredo@hotmail.com>
2015-12-22 15:38:46 -0500

    mensajes del chats

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -26,6 +26,8 @@ class Api extends REST_Controller {
 	   echo "";
     }
 	
+	/************** Pantalla MESSAGES ******************/
+	
 	/**
 	 * Obtiene la lista de chats del usuario
 	 */
@@ -41,12 +43,10 @@ class Api extends REST_Controller {
 				$result[0]->display_name = $user[0]->display_name;
 				$result[0]->blockYour = $user[0]->status;
 				$result[0]->blockMe = $item->status;
-				
 				if(count($result) > 0){
 					$array2 = json_decode(json_encode($result[0]),true);
 					array_push($chats, $array2);
 				}
-				
 			}
 			usort($chats, array($this, "ordenar")); 
 			$message = array('success' => true, 'items' => $chats );
@@ -54,10 +54,11 @@ class Api extends REST_Controller {
         $this->response($message, 200);
 	}
 	
-	
 	public function ordenar($a, $b) {
 		return strtotime($b['sent_at']) - strtotime($a['sent_at']);
 	}
+	
+	/************** Pantalla MESSAGE ******************/
 	
 	/**
 	 * Obtiene los mensajes por channel
@@ -65,7 +66,8 @@ class Api extends REST_Controller {
 	public function getChatMessages_get(){
 		$message = $this->verifyIsSet(array('idApp'));
 		if ($message == null) {
-			$months = array('', 'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
+			$months = array('', 'Enero','Febrero','Marzo','Abril','Mayo','Junio',
+			'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
 			$dias = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');
             $messages = $this->api_db->getMessagesByChannel($this->get('channelId'));
 			$dateBefore = "";
@@ -77,7 +79,9 @@ class Api extends REST_Controller {
 				}
 				$fechaD = $dias[date('N', strtotime($item->sent_at)) - 1];
 				$item->dia = $fechaD;
-				$item->fechaFormat = date('d', strtotime($item->sent_at)) . ' de ' . $months[date('n', strtotime($item->sent_at))] . ' del ' . date('Y', strtotime($item->sent_at));
+				$item->fechaFormat = date('d', strtotime($item->sent_at)) . ' de ' . 
+					$months[date('n', strtotime($item->sent_at))] . ' del ' . 
+					date('Y', strtotime($item->sent_at));
 				$date = date_create($item->sent_at);
 				$item->hora = date_format($date, 'g:i A');
 				if($dateBefore != $item->dateOnly){
@@ -93,48 +97,44 @@ class Api extends REST_Controller {
 	}
 	
 	/**
-	 * Guarda el mensaje
+	 * Guarda los chats
 	 */
 	public function saveChat_get(){
 		$message = $this->verifyIsSet(array('idApp'));
 		if ($message == null) {
-			
-			$months = array('', 'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
+			$months = array('', 'Enero','Febrero','Marzo','Abril','Mayo','Junio',
+				'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
 			$dias = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');
-			
 			$insert = array(
 				'sender_id' 	=> $this->get('idApp'),
 				'channel_id' 	=> $this->get('channelId'),
 				'message' 		=> $this->get('message')
 				//'sent_at'			=> $this->get('dateM')
 			);
-			
             $chat = $this->api_db->InsertMessageOfChat($insert);
 			$user = $this->api_db->getUserChat($this->get('channelId'),$this->get('idApp'));
+			$noRead = $this->api_db->getChatNoReadById($this->get('channelId'),$this->get('idApp'));
+		
 			foreach($chat as $item){
 				$date = date_create($item->sent_at);
 				$item->hora = date_format($date, 'g:i A');
-				$item->fechaFormat = date('d', strtotime($item->sent_at)) . ' de ' . $months[date('n', strtotime($item->sent_at))] . ' del ' . date('Y', strtotime($item->sent_at));
+				$item->fechaFormat = date('d', strtotime($item->sent_at)) . ' de ' . 
+					$months[date('n', strtotime($item->sent_at))] . ' del ' . 
+					date('Y', strtotime($item->sent_at));
 				$item->idUSer = $user[0]->idUSer;
 				$item->display_name = $user[0]->display_name;
-				
+				$item->NoRead = $noRead[0]->NoRead;
 				if($user[0]->playerId != '0' || $user[0]->playerId != 0){
 					$this->SendNotificationPush($user[0]->playerId,json_encode($chat),"1");
 				}
 			}
-			
-			//foreach
-			//
-			
 			$message = array('success' => true, 'items' => $chat );
-			
-			
         }
         $this->response($message, 200);
 	}
 	
 	/**
-	 * bloquea el chat
+	 * bloquea o desbloquea los chats
 	 */
 	public function blokedChat_get(){
 		$message = $this->verifyIsSet(array('idApp'));
@@ -143,22 +143,33 @@ class Api extends REST_Controller {
 			if($this->get('status') == "closed"){
 				$status = "open";
 			}
-			
 			$update = array(
 				'channel_id'	=> $this->get('channelId'),
 				'user_id'		=> $this->get('idApp'),
 				'status' 		=> $status
 			);
-			
 			$this->api_db->updateStatusChat($update);
 			$message = array('success' => true, 'message' => "chat bloqueado", 'status' => $status );
         }
         $this->response($message, 200);
 	}
     
-    
-    
-    
+	/**
+	 * Cambia los status de los mensajes
+	 */
+    public function changeStatusMessages_get(){
+		$message = $this->verifyIsSet(array('idApp'));
+		if ($message == null) {
+			$update = array(
+				'id' 			=> $this->get('idMessage'),
+				'channel_id'	=> $this->get('channelId'),
+				'sender_id'		=> $this->get('idApp')
+			);
+			//$this->api_db->changeStatusMessages($update);
+			$message = array('success' => true, 'message' => "mensajes leidos" );
+        }
+        $this->response($message, 200);
+	}
     
     /************** Pantalla HOME ******************/
     
@@ -174,7 +185,6 @@ class Api extends REST_Controller {
         $message = array('success' => true, 'items' => $items );
         $this->response($message, 200);
 	}
-	
 	
 	/************** metodo generico ******************/
 	
@@ -194,20 +204,15 @@ class Api extends REST_Controller {
 	 */
 	public function SendNotificationPush($playerId, $items, $type){
 		
-		//$idMSGNew = $idMSGNew . "";
-		
 		$userID = [$playerId]; 
 		if($type == 1){
 			$massage = "Nuevo mensaje";
 		}
-	  
 		$content = array(
 			"en" => $massage
 		);
-    
 		$fields = array(
 		'app_id' => "b7f8ee34-cf02-4671-8826-75d45b3aaa07",
-		//'included_segments' => array('All'),
 		'include_player_ids' => $userID,
 		'data' => array("type" => $type, "item" => $items),
 		'isAndroid' => true,
@@ -215,8 +220,6 @@ class Api extends REST_Controller {
 		);
     
 		$fields = json_encode($fields);
-		//print("\nJSON sent:\n");
-		// print($fields);
 		
 		$ch = curl_init();
 	
@@ -232,15 +235,8 @@ class Api extends REST_Controller {
 		$response = curl_exec($ch);
 		$return["allresponses"] = $response;
 		$return = json_encode($return);
-  
-		$findme   = 'error';
-		$pos = strpos($return, $findme);
-	
-		if ($pos === false) {
-		}
 	
 		curl_close($ch);
-		
 	}
 	
 }
--- application/models/api_db.php
+++ application/models/api_db.php
@@ -6,6 +6,8 @@ Class api_db extends CI_MODEL
         parent::__construct();
     }
 	
+	/************** Pantalla MESSAGES ******************/
+	
 	/**
 	 * obtenemos la lista de mensajes chats del usuario
 	 */
@@ -20,14 +22,13 @@ Class api_db extends CI_MODEL
 	 * obtenemos la lista de mensajes chats del usuario
 	 */
 	public function getListMessageChat($channelId,$id){
-		$this->db->select('wp_bp_chat_messages.id, wp_bp_chat_messages.channel_id, wp_bp_chat_messages.message');
+		$this->db->select('wp_bp_chat_messages.id, wp_bp_chat_messages.channel_id, wp_bp_chat_messages.message, wp_bp_chat_messages.status_message');
 		$this->db->select('wp_bp_chat_messages.sender_id, wp_bp_chat_messages.sent_at, wp_bp_chat_messages.sent_at');
-		//$this->db->select('wp_users.id as idUSer, wp_users.display_name');
+		$this->db->select('(select count(*) from wp_bp_chat_messages where wp_bp_chat_messages.channel_id = ' . 
+			$channelId . ' and wp_bp_chat_messages.status_message = 0 and wp_bp_chat_messages.sender_id != ' . $id . ' ) as NoRead ');
 		$this->db->from('wp_bp_chat_messages');
-		//$this->db->join('wp_users', 'wp_users.id = wp_bp_chat_messages.sender_id');
 		$this->db->where('wp_bp_chat_messages.channel_id = ', $channelId);
-		$this->db->order_by('wp_bp_chat_messages.sent_at', 'desc'); 
-		//$this->db->limit(1);
+		$this->db->order_by('wp_bp_chat_messages.sent_at', 'desc');
         return $this->db->get()->result();
 	}
 	
@@ -46,10 +47,25 @@ Class api_db extends CI_MODEL
 	}
 	
 	/**
+	 * Obtiene los mensajes no leidos por id
+	 */
+	function getChatNoReadById($channelId, $id){
+		$this->db->select('count(*) as NoRead');
+		$this->db->from('wp_bp_chat_messages');
+		$this->db->where('wp_bp_chat_messages.channel_id = ', $channelId);
+		$this->db->where('wp_bp_chat_messages.status_message = 0');
+		$this->db->where('wp_bp_chat_messages.sender_id != ', $id);
+        return $this->db->get()->result();
+	}
+	
+	/************** Pantalla MESSAGE ******************/
+	
+	/**
 	 * obtenemos la lista de mensajes del canal
 	 */
 	public function getMessagesByChannel($channelId){
-		$this->db->select('wp_bp_chat_messages.id, wp_bp_chat_messages.channel_id, wp_bp_chat_messages.sender_id, wp_bp_chat_messages.message');
+		$this->db->select('wp_bp_chat_messages.id, wp_bp_chat_messages.channel_id, wp_bp_chat_messages.sender_id');
+		$this->db->select('wp_bp_chat_messages.message, wp_bp_chat_messages.status_message');
 		$this->db->select('wp_bp_chat_messages.sent_at, date(wp_bp_chat_messages.sent_at) as dateOnly');
 		$this->db->from('wp_bp_chat_messages');
 		$this->db->where('wp_bp_chat_messages.channel_id = ', $channelId);
@@ -61,7 +77,6 @@ Class api_db extends CI_MODEL
 	 * inserta el mensaje del chat
 	 */
 	function InsertMessageOfChat($data){
-		
 		$this->db->insert('wp_bp_chat_messages', $data);
 		$id = $this->db->insert_id();
 		
@@ -79,6 +94,17 @@ Class api_db extends CI_MODEL
 		$this->db->where('channel_id', $data['channel_id']);
 		$this->db->where('user_id', $data['user_id']);
         $this->db->update('wp_bp_chat_channel_users', $data);
+	}
+	
+	/**
+	 * cambia es status de los mensajes del chats
+	 */
+	function changeStatusMessages($data){
+		$this->db->set('status_message', 1); 
+		$this->db->where('channel_id = ', $data['channel_id']);
+		$this->db->where('sender_id != ', $data['sender_id']);
+		$this->db->where('id <= ', $data['id']);
+        $this->db->update('wp_bp_chat_messages');
 	}
     
     
